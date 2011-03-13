b9d94e5a131e03bb156a447f58ad427f7f23897d
Gabor Szanto <hello@szantogabor.com>
2011-03-13 18:13:06 +0100

    Delete untracked js files

--- sites/default/files/languages/hu_e53dacbc058a09d985034ac7034a2430.js
+++
@@ -1,1 +0,0 @@
-Drupal.locale = { 'pluralFormula': function($n) { return Number(($n!=1)); }, 'strings': {"Unspecified error":"Meghat\u00e1rozatlan hiba","Group node":"Csoport","Edit":"Szerkeszt\u00e9s","Translate Text":"Sz\u00f6veg ford\u00edt\u00e1sa","An HTTP error @status occured.":"HTTP @status hiba fordult el\u0151."} };
